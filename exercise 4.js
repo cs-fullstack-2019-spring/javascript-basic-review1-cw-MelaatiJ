868ce312cfb261d8722f93bd525e0a0678606143
Melaati Jayah <fathimajayah876@gmail.com>
2019-01-11 13:11:18 -0600

    classwork for review

--- exercise 4.js
+++ exercise 4.js
@@ -1,6 +1,12 @@
 //How to combine math  problems, ask users for a question and log it with strings
 
-var FTEMP = parseInt(prompt("What is the temperture?"));
+
+do
+{
+    var FTEMP = parseInt(prompt("Enter the temperature!"));
+}
+while (isNaN(FTEMP));
 var CTEMP = Math.round((FTEMP - 32) * 5 / 9);
-console.log(FTEMP + " Fahrenheit equals " + CTEMP + " in Celcius");
+console.log(FTEMP + " degrees fahrenheit equals " + CTEMP + " degrees in celcius");
+
 
